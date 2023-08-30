1549010c8f4bd27a99c522f42de12a7c76482ebe
Nandha Kumar <nandhakumar@instrive.com>
2023-08-30 17:38:29 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '86aq5lrpzyiroa', // your App ID
+      'clientSecret': 'OihZwxlKBFV8j6H8', // your App Secret
+      'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+    }
+  }
+  
